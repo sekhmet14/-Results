a4a1782a46af8a4ac66f3d14d532f83154eaae33
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-08-01 23:01:52 +0700

    задание метода вывода строки

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string[] arr1 = new string[5] {"1234", "1567", "hello", "world", "2"};
+string[] arr2 = new string[arr1.Length];
+
+void  HelperArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
